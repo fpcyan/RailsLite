22cd750e02f4d2f27ed0349d8f4bc929111f9f61
fpcyan <fionapwconn@gmail.com>
2015-11-30 16:41:47 -0500

    Moved controllers to their own directory

---
+++ controllers/cats_2_controller.rb
@@ -0,0 +1,12 @@
+require_relative '../lib/controller_base'
+require_relative '../lib/router'
+
+$cats = [
+  { id: 1, name: "Curie" },
+  { id: 2, name: "Markov" }
+]
+class Cats2Controller < ControllerBase
+  def index
+    render_content($cats.to_s, "text/text")
+  end
+end
---
+++ controllers/statuses_controller.rb
@@ -0,0 +1,18 @@
+require_relative '../lib/controller_base'
+require_relative '../lib/router'
+
+$statuses = [
+  { id: 1, cat_id: 1, text: "Curie loves string!" },
+  { id: 2, cat_id: 2, text: "Markov is mighty!" },
+  { id: 3, cat_id: 1, text: "Curie is cool!" }
+]
+
+class StatusesController < ControllerBase
+  def index
+    statuses = $statuses.select do |s|
+      s[:cat_id] == Integer(params[:cat_id])
+    end
+
+    render_content(statuses.to_s, "text/text")
+  end
+end
